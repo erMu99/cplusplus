3a3d6bd55ece62621bbadbe4f5ddadc55dd9ab47
xialin <1441292675@qq.com>
2023-05-16 11:55:32 +0800

    测试代码

---
+++ Hash/Test.cpp
@@ -0,0 +1,19 @@
+//#include "HashTable.h"
+
+#include "UnorderedSet.h"
+#include "UnorderedMap.h"
+#include <unordered_set>
+#include <unordered_map>
+int main()
+{
+	//closeHash::Test1();
+	//bucketHash::Test_Hash();
+
+	//nb::test_unordered_set();
+	//nb::test_unordered_map();
+
+	//nb::test_us_1();
+	nb::test_us_2();
+
+	return 0;
+}
